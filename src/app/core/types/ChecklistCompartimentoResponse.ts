1fb20f5ce1115b2a5aa5cc472cd97d5289520e53
Douglas Ferreira da Silva <doug.ferreira.dfs@gmail.com>
2025-06-13 12:20:41 -0300

    Criado o type ChecklistCompartimentoResponse

---
+++ src/app/core/types/ChecklistCompartimentoResponse.ts
@@ -0,0 +1,39 @@
+import { CompartimentoResumo } from "./CompartimentoResponse";
+import { Ordenacao } from "./Ordenacao";
+import { Paginacao } from "./Paginacao";
+
+export interface ChecklistCompartimentoDetalhado {
+    id: number,
+    checklistAmbiente: ChecklistCompartimentoDetalhado,
+    compartimento: CompartimentoResumo,
+    dataHoraPreenchimentoEntrada: Date,
+    dataHoraPreenchimentoSaida: Date,
+    executorPreenchimentoEntrada: string,
+    executorPreenchimentoSaida: string,
+    status: string
+}
+
+export interface ChecklistCompartimentoResumo {
+    id: number,
+    checklistAmbiente: ChecklistCompartimentoDetalhado,
+    compartimento: CompartimentoResumo,
+    dataHoraPreenchimentoEntrada: Date,
+    dataHoraPreenchimentoSaida: Date,
+    executorPreenchimentoEntrada: string,
+    executorPreenchimentoSaida: string,
+    status: string
+}
+
+export interface PaginaChecklistAmbiente {
+    content: ChecklistCompartimentoResumo[];
+    pageable: Paginacao;
+    totalElements: number;
+    totalPages: number;
+    last: boolean;
+    first: boolean;
+    numberOfElements: number;
+    size: number;
+    number: number;
+    sort: Ordenacao;
+    empty: boolean;
+}
