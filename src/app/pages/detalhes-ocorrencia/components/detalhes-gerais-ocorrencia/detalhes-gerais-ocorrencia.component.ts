86d634f94250644abee19bdd79274a4dd2d127c6
Douglas Ferreira da Silva <doug.ferreira.dfs@gmail.com>
2025-07-23 10:47:08 -0300

    Adicionao links para navegação para ambeinte, compartimento e checklist de ambiente na pagina de detalhes da ocorrência

--- src/app/pages/detalhes-ocorrencia/components/detalhes-gerais-ocorrencia/detalhes-gerais-ocorrencia.component.ts
+++ src/app/pages/detalhes-ocorrencia/components/detalhes-gerais-ocorrencia/detalhes-gerais-ocorrencia.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { OcorrenciaDetalhado } from '../../../../core/types/OcorrenciaResponse';
 
 @Component({
   selector: 'app-detalhes-gerais-ocorrencia',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './detalhes-gerais-ocorrencia.component.html',
   styleUrl: './detalhes-gerais-ocorrencia.component.css'
 })
